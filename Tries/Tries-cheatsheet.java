5d3fbabdd40a656d61f96091c27157eb95115b1a
SarveshGuptaPythonist <61269101+SarveshGuptaPythonist@users.noreply.github.com>
2022-01-15 15:58:12 +0530

    Update Tries-cheatsheet.java

--- Tries/Tries-cheatsheet.java
+++ Tries/Tries-cheatsheet.java
@@ -102,3 +102,53 @@ class Trie {
  * boolean param_2 = obj.search(word);
  * boolean param_3 = obj.startsWith(prefix);
  */
+public static class Node{
+        private Node[] children = new Node[26];
+        private boolean isEnd = false;
+
+        public boolean contains(char ch){
+            return (children[ch - 'a'] != null);
+        }
+
+        public Node get(char ch){
+            return children[ch - 'a'];
+        }
+
+        public void set(char ch){
+            children[ch - 'a'] = new Node();
+        }
+
+        public boolean getEnd(){
+            return isEnd;
+        }
+
+        public void setEnd(){
+            isEnd = true;
+        }
+    }
+    String res;
+    public void insert(String word, Node root) {
+        Node curr = root;
+        for(int i=0; i<word.length(); i++){
+            char ch = word.charAt(i);
+            if(curr.contains(ch) == false)
+                curr.set(ch);
+            curr = curr.get(ch);
+        }
+        curr.setEnd();
+    }
+    public String search(String word, Node root) {
+        Node curr = root;
+        for(int i=0; i<word.length(); i++){
+            char ch = word.charAt(i);
+            if(curr.getEnd() == true) {
+                return word.substring(0, i);
+            }
+            if(curr.contains(ch) == false)
+                return word;
+
+            curr = curr.get(ch);
+        }
+
+        return word;
+    }
